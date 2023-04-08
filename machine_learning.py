fa637576f50332014b625660c30f5947dd49e482
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-04-09 00:26:05 +0530

    linear regression

---
+++ machine_learning.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+# df = pd.read_csv('C:\\Users\\shiva\\OneDrive\\Desktop\\SHIVANG.csv')
+# print(df)
+# plt.xlabel('area')
+# plt.ylabel('price')
+# plt.scatter(df.area,df.price, color = 'red', marker='+')
+# # # plt.show()
+#linear regression in single variable
+# reg = linear_model.LinearRegression()
+# reg.fit(df[['year']],df.price)
+# y = mx + c
+# a = reg.predict([[2800]])
+# print(a)                                       tells x
+# b = reg.intercept_
+# print(b)                                         tells c
+# c = reg.coef_
+# print(c)                                     tells m
+# print(83.78378378*2800 + 337297.2972972974 )
+# d = pd.read_csv('C:\\Users\\shiva\\OneDrive\\Desktop\\question.csv')
+# print(d)
+# e = reg.predict(d)                         21-27 homework question to predict price in following year
+# print(e)
+# d['prices'] = e
+# print(d)
+# d.to_csv('C:\\Users\\shiva\\OneDrive\\Desktop\\sagar.csv',index=False)
+# plt.xlabel('area')
+# plt.ylabel('price')
+# plt.scatter(df.area,df.price, color = 'red', marker='+')
+# plt.plot(df.area,reg.predict(df[['area']]),color = 'blue')           draw graph and tells whether price is low or up
+# plt.show()
+
+#linear regression in multiple variables
+
+
+
+
