601c95e39401757f55ea40a77dabd38db38e72a9
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-08-03 21:30:35 +0530

    "added  pickle and joblib "

--- machine_learning.py
+++ machine_learning.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import linear_model
+import pickle
+import joblib 
 df = pd.read_csv('C:\\Users\\shiva\\OneDrive\\Desktop\\DS\\SHIVANG.csv')
 print(df)
 plt.xlabel('area')
@@ -56,4 +58,31 @@ print(digits.data[0])
 plt.gray()
 a = plt.matshow(digits.images[0])
 a = plt.matshow(digits.images[0])
+# def gradient_descent(x,y):
+#     m_curr = b_curr = 0
+#     iterations = 1000
+#     n = len(x)
+#     learning_rate = 0.001
+#     for i in range(iterations):
+#         y_predicted = m_curr * x + b_curr
+#         cost = (1/n) * sum([val**2 for val in (y-y_predicted)])
+#         md = -(2/n) * sum(x*(y-y_predicted))
+#         bd = -(2/n) * sum(y-y_predicted)
+#         m_curr - m_curr - learning_rate * md
+#         b_curr = b_curr - learning_rate * bd
+#         print ("m {}, b {} ,cost {} iteration {}".format(m_curr,b_curr,cost,i))
 
+# x = np.array([1,2,3,4,5])
+# y = np.array([5,7,9,11,13])
+
+# gradient_descent(x,y)
+# with open('reg_pickle','wb') as f:
+#     pickle.dump(reg,f)
+# with open('reg_pickle','rb') as f:
+#     mp = pickle.load(f)
+# c = mp.predict([[5000]])
+# print(c)
+# joblib.dump(reg,'reg_joblib')
+# mj = joblib.load('reg_joblib')
+# a = mj.predict([[5000]])
+# print(a)
