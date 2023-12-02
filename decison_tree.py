90413c2308af1e7c49be9aee622df12b8141ac20
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-12-02 23:30:01 +0530

    "did some correction due to which model accuracy decreased T_T

--- decison_tree.py
+++ decison_tree.py
@@ -1,20 +1,29 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 df = pd.read_csv("salaries.csv")
 a = df.head(10)
 print(a)
-inputs = df.drop('salary_more_then_100k' , axis='columns')
-target = ['salary_more_then_100k']
-print(inputs)
-from sklearn.preprocessing import LabelEncoder
+print("Column names:", df.columns)
+target_column_name = 'salary_more_then_100k'
+inputs = df.drop(target_column_name, axis='columns')
+target = df[target_column_name]
 le_company = LabelEncoder()
 le_job = LabelEncoder()
 le_degree = LabelEncoder()
+
 inputs['company_n'] = le_company.fit_transform(inputs['company'])
-
-inputs['job_n'] = le_company.fit_transform(inputs['job'])
-
-inputs['degree_n'] = le_company.fit_transform(inputs['degree'])
-f = inputs.head(19)
+inputs['job_n'] = le_job.fit_transform(inputs['job'])
+inputs['degree_n'] = le_degree.fit_transform(inputs['degree'])
+f = inputs.head(10)
 print(f)
-inputs_n = inputs.drop(['company','job','degree'],axis='columns')
+inputs_n = inputs.drop(['company', 'job', 'degree'], axis='columns')
 print(inputs_n)
+X_train, X_test, y_train, y_test = train_test_split(inputs_n, target, test_size=0.2, random_state=42)
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+train_accuracy = model.score(X_train, y_train)
+print("Training Accuracy:", train_accuracy)
+test_accuracy = model.score(X_test, y_test)
+print("Testing Accuracy:", test_accuracy)
