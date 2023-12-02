626553e694570ddf333588261fdba44662fc4655
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-12-02 23:21:23 +0530

    "did some label encoding to solve confusion"

--- decison_tree.py
+++ decison_tree.py
@@ -5,3 +5,16 @@ print(a)
 inputs = df.drop('salary_more_then_100k' , axis='columns')
 target = ['salary_more_then_100k']
 print(inputs)
+from sklearn.preprocessing import LabelEncoder
+le_company = LabelEncoder()
+le_job = LabelEncoder()
+le_degree = LabelEncoder()
+inputs['company_n'] = le_company.fit_transform(inputs['company'])
+
+inputs['job_n'] = le_company.fit_transform(inputs['job'])
+
+inputs['degree_n'] = le_company.fit_transform(inputs['degree'])
+f = inputs.head(19)
+print(f)
+inputs_n = inputs.drop(['company','job','degree'],axis='columns')
+print(inputs_n)
