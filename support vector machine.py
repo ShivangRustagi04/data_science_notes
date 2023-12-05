7198880426c8be7cadfebd616e1d9dcfc5b268d3
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-12-05 21:06:30 +0530

    "using scatter graph shown 2 plots in one 1 graph"

--- support vector machine.py
+++ support vector machine.py
@@ -1,12 +1,19 @@
 import pandas as pd 
 from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns= iris.feature_names)
 df['target'] = iris.target
 print(df.head(15))
 newdf = df[df.target==2].head()
 print(newdf)
-
+df0 = df[df.target==0]
+df1 = df[df.target==1]
+df2 = df[df.target==2]
+plt.scatter(df0['sepal length (cm)'], df1['sepal width (cm)'], color='red', marker='+')
+plt.scatter(df1['sepal length (cm)'], df0['sepal width (cm)'], color='green', marker='.')
+plt.legend()
+plt.show()
 
 
 
