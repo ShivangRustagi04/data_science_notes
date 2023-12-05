f88003a8a08ae8c12f450af6c808991db863321f
ShivangRustagi04 <shivangrustagi04@gmail.com>
2023-12-05 20:55:22 +0530

    "finding target name by thier numbers"

--- support vector machine.py
+++ support vector machine.py
@@ -2,7 +2,10 @@ import pandas as pd
 from sklearn.datasets import load_iris
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns= iris.feature_names)
+df['target'] = iris.target
 print(df.head(15))
+newdf = df[df.target==2].head()
+print(newdf)
 
 
 
